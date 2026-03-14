bb74f0e4afcd1b9a6df31ba10d40979022c33ce0
smmlt <kbodia80@gmail.com>
2026-03-14 09:06:21 +0100

    Initial commit

---
+++ constants/FirebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgV9Suxm8wmXBI6rEPAHKUQTft1WCZ08w",
+  authDomain: "fitness-app-52c2e.firebaseapp.com",
+  projectId: "fitness-app-52c2e",
+  storageBucket: "fitness-app-52c2e.firebasestorage.app",
+  messagingSenderId: "158279989811",
+  appId: "1:158279989811:web:8d4f356b4e3fe86a310146"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
