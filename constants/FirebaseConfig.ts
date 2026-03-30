3ba3e12d060131c5c75ea4977e941c36758ebd6b
smmlt <kbodia80@gmail.com>
2026-03-30 12:46:51 +0200

    .env

--- constants/FirebaseConfig.ts
+++ constants/FirebaseConfig.ts
@@ -8,14 +8,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDgV9Suxm8wmXBI6rEPAHKUQTft1WCZ08w",
-  authDomain: "fitness-app-52c2e.firebaseapp.com",
-  projectId: "fitness-app-52c2e",
-  storageBucket: "fitness-app-52c2e.firebasestorage.app",
-  messagingSenderId: "158279989811",
-  appId: "1:158279989811:web:8d4f356b4e3fe86a310146"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
