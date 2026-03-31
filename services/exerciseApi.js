205fd6d8617162bb1331f273dd2a66b8c1f53d86
smmlt <kbodia80@gmail.com>
2026-03-31 11:49:14 +0200

    .env.local .env.example
    .env.local
    .env.example

--- services/exerciseApi.js
+++ services/exerciseApi.js
@@ -1,40 +1,44 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const API_KEY = '44630b0df2mshb6c347e6cb811bdp12a0c6jsnbae03fd981c1';
-const BASE_URL = 'https://exercisedb.p.rapidapi.com';
+const API_KEY = process.env.EXPO_PUBLIC_RAPIDAPI_KEY;
+const API_HOST = process.env.EXPO_PUBLIC_RAPIDAPI_HOST;
+const BASE_URL = process.env.EXPO_PUBLIC_RAPIDAPI_BASE_URL;
 
-export async function fetchExercises(limit = 10) {
-  try {
-    const response = await axios.get(`${BASE_URL}/api/v1/exercises`, {
-      headers: {
-        'X-RapidAPI-Key': API_KEY,
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-      },
-      params: { 
-        limit: limit
-      }
-    });
-    return response.data;
-  } catch (err) {
-    console.error('Помилка API:', err.message);
-    return [];
-  }
+export async function fetchExercisesByBodyPart(bodyPartName)
+{
+    try {
+        const response = await axios.get(`${BASE_URL}/exercises/bodyPart/${bodyPartName}`, {
+            params: { bodyPartName },
+            headers: {
+                'x-rapidapi-key': API_KEY,
+                'x-rapidapi-host': API_HOST
+            }
+        })
+
+        return (response).data;
+    }
+    catch (e)
+    {
+        console.error(`api error ${e.message}`)
+        return []
+    }
 }
 
-export async function fetchExercisesByBodyPart(bodyPartName) {
-  try {
-    const response = await axios.get(`${BASE_URL}/api/v1/bodyparts/${bodyPartName}/exercises`, {
-      headers: {
-        'X-RapidAPI-Key': API_KEY,
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-      },
-      params: { 
-        bodyPartName: bodyPartName
-      }
-    });
-    return response.data;
-  } catch (err) {
-    console.error('Помилка API:', err.message);
-    return [];
-  }
+export async function fetchExercises()
+{
+    try {
+        const response = await axios.get(`${BASE_URL}/exercises`, {
+            headers: {
+                'x-rapidapi-key': API_KEY,
+                'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+            }
+        })
+
+        return response.data;
+    }
+    catch (e)
+    {
+        console.error(`api error ${e.message}`)
+        return []
+    }
 }
